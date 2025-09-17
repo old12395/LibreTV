ca9116e02aadc0ae96f5fdd1232a6ec9d7482929
old12395 <old12395@gmail.com>
2025-09-17 11:11:09 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,6 +3,19 @@ const CUSTOMER_SITES = {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
     }
+    const CUSTOMER_SITES = {
+    qiqi: {
+        api: 'https://www.qiqidys.com/api.php/provide/vod',
+        name: '--资源',
+    }
+};
+
+// 调用全局方法合并
+if (window.extendAPISites) {
+    window.extendAPISites(CUSTOMER_SITES);
+} else {
+    console.error("错误：请先加载 config.js！");
+}
 };
 
 // 调用全局方法合并
